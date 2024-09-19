a291ba23e682c17c6d41e252ec0f9385dede9453
Ashvin Antyah <a.antyah@gmail.com>
2024-09-19 14:17:24 +0200

    start editing the completed section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-       'custom-green': 'rgb(216, 219, 47)',
+       'custom-green': 'rgb(215, 218, 47)',
        'custom-dark-blue' :'rgb(19, 48, 64)',
        'custom-grey':'rgb(127 143 149)'
       }
