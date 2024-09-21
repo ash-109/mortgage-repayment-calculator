67d7ed3b40e6987566a3b05b80c6891881a56a04
Ashvin Antyah <a.antyah@gmail.com>
2024-09-21 20:21:01 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
+
+const btnClear = document.getElementById('btn_clear')
+const mortgageAmount= document.getElementById('mortgage_amount')
+const mortgageTerm = document.getElementById('mortgage_term')
+const interestRate = document.getElementById('interest_rate')
+const monthlyRepayment= document.getElementById('monthly_repayment')
+const totalRepayment = document.getElementById('total_repayment')
+
 let result = document.getElementById('results')
-console.log(result);
